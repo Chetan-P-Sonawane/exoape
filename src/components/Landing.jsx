9c221774d289baec777fbff0c124f032a8b6e3f5
Chetan Pravin Sonawane <chetanps131@gmail.com>
2024-03-30 18:01:07 +0530

    work completed

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,16 +9,16 @@ const Landing = () => {
        <div className="w-full absolute top-0">
         <div className=' text max-w-screen-xl h-full mx-auto px-7 sm:px-14 text-white'>
           <div className='para mt-72 sm:mt-[29rem]'>
-            <p className='text-[3.4vw] sm:text-[1.56vw] font-semibold '>Global digital design studio partnering with</p>
-            <p className='text-[3.4vw] sm:text-[1.56vw] font-semibold '>brands and businesses that create exceptional</p>
-            <p className='text-[3.4vw] sm:text-[1.56vw] font-semibold '>experiences where people live, work, and unwind.</p>
+            <p className='text-[3.4vw] sm:text-[1.56vw] tracking-[0.5px] font-semibold '>Global digital design studio partnering with</p>
+            <p className='text-[3.4vw] sm:text-[1.56vw] tracking-[0.5px] font-semibold '>brands and businesses that create exceptional</p>
+            <p className='text-[3.4vw] sm:text-[1.56vw] tracking-[0.5px] font-semibold '>experiences where people live, work, and unwind.</p>
           </div>
           <div className="headings mt-[5vw] sm:mt-[5vw] sm:-ml-[1.5vw]">
-            <h1 className="text-6xl sm:text-[18vw] tracking-tighter leading-none">Digital</h1>
-            <h1 className="text-6xl sm:text-[18vw] tracking-tighter leading-none">Design</h1>
-            <h1 className="text-6xl sm:text-[18vw] tracking-tighter leading-none">Experience</h1>
+            <h1 className="text-[15vw] sm:text-[18vw] tracking-tight leading-none">Digital</h1>
+            <h1 className="text-[15vw] sm:text-[18vw] tracking-tight leading-none">Design</h1>
+            <h1 className="text-[15vw] sm:text-[18vw] tracking-tight leading-none">Experience</h1>
           </div>
-          <div className="para2 mt-[10vw] sm:mt-[9vw] sm:w-1/2 text-[3.4vw] sm:text-[1.51vw] font-semibold">
+          <div className="para2 mt-[10vw] sm:mt-[9vw] sm:w-1/2 text-[3.4vw] sm:text-[1.51vw] tracking-[0.5px] font-semibold">
             <p>We help experience-driven companies thrive by making their audience feel the refined intricacies of their brand and product in the digital space. Unforgettable journeys start with a click.</p>
             <a className='border-b-[.3px] sm:border-b-[1px] text-sm sm:text-lg border-zinc-100 pb-1 mt-[8vw] sm:mt-24 inline-block' href="/">The Studio</a>
           </div>
